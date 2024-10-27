92cabed665c5fdd5135107b7f47286e9d2f82da1
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-27 13:36:57 +0530

    Putting heading and status of command whether it is success or failure for full backend file

--- backend.sh
+++ backend.sh
@@ -1,29 +1,52 @@
 echo Disable default NodeJs Version module
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>/tmp/expense.log
 echo $?
 
 echo Enable NodeJs module for V20
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>/tmp/expense.log
 echo $?
 
 echo Install NodeJs
-dnf install nodejs -y
+dnf install nodejs -y &>>/tmp/expense.log
 echo $?
 
-useradd expense
-cp backend.service /etc/systemd/system/backend.service
+echo Adding application user
+useradd expense &>>/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
+echo $?
 
-rm -rf /app
+echo Clear the old content
+rm -rf /app &>>/tmp/expense.log
+echo $?
 
-mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
-unzip /tmp/backend.zip
-cd /app
-npm install
+echo Create app directory
+mkdir /app &>>/tmp/expense.log
+echo $?
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
-dnf install mysql -y
-mysql -h 172.31.82.191 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo Download the app content
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
+echo $?
+
+echo Extract app content
+cd /app &>>/tmp/expense.log
+unzip /tmp/backend.zip &>>/tmp/expense.log
+echo $?
+
+echo Download NodeJs dependencies
+cd /app &>>/tmp/expense.log
+npm install &>>/tmp/expense.log
+echo $?
+
+echo Start backend service
+systemctl daemon-reload &>>/tmp/expense.log
+systemctl enable backend &>>/tmp/expense.log
+systemctl start backend &>>/tmp/expense.log
+echo $?
+
+echo Install mysql client
+dnf install mysql -y &>>/tmp/expense.log
+echo $?
+
+echo Load schema
+mysql -h 172.31.82.191 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
+echo $?
