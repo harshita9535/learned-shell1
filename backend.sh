33fe862d84f659d1149dcd973d43d9c73002d014
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-27 09:54:02 +0530

    edited and added backendservice file and expense.conf file

--- backend.sh
+++ backend.sh
@@ -2,12 +2,15 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
+rm -rf /app
+
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
 unzip /tmp/backend.zip
 cd /app
 npm install
+cp /etc/systemd/system/backend.service
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
