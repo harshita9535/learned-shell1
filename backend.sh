2c947ad7ae41a0a71532d52d07d935f16f59c155
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-27 09:41:27 +0530

    Create backend script

---
+++ backend.sh
@@ -0,0 +1,15 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd expense
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+cd /app
+unzip /tmp/backend.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
+dnf install mysql -y
+mysql -h <172.31.82.191> -uroot -pExpenseApp@1 < /app/schema/backend.sql
