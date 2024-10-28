b0f70605df01d761ff3b2757a9a9b7e0946bc9ba
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-28 15:09:01 +0530

    if condition

--- if.sh
+++ if.sh
@@ -1,8 +1,8 @@
 simple if
 
 a=$1
-if [ $a -gt 100 ]; then
-  echo "$a is greater than 100"
+if [ $1 -gt 100 ]; then
+  echo "$1 is greater than 100"
 else
-  echo "$a is less than 100"
+  echo "$1 is less than 100"
 fi
